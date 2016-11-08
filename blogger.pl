715187d09bcb8c2d3afb24636d0bf1b02860cff7
darkmind <nickolay.malkov@gmail.com>
2016-11-08 17:33:46 +0200

    Add list of page ids

--- blogger.pl
+++ blogger.pl
@@ -17,57 +17,22 @@ Readonly::Scalar my $CLIENT_ID
 Readonly::Scalar my $CLIENT_SEC => 'ezZHWiVYfS9vq3ON6IYzuKN6';
 Readonly::Scalar my $BLOG_ID    => '7678772071518232421';
 Readonly::Scalar my $HTML_PATH  => "$Bin/_build/html/documentation";
+Readonly::Scalar my $PAGE_IDS   => "$Bin/page_ids.txt";
 
 use utf8;
 binmode(STDOUT,':utf8');
 
 my $token = $ARGV[0];
 if ( !defined($token) ) {
-    my $client = REST::Client->new();
-    $client->setHost('https://accounts.google.com');
-    $client->POST(
-        'o/oauth2/v2/auth?'
-        . 'scope=https://www.googleapis.com/auth/blogger&'
-        . 'redirect_uri=http://localhost:8080&'
-        . 'response_type=code&'
-        . "client_id=$CLIENT_ID"
-    );
-    my $path = $client->responseHeader('Location');
-    system( 'google-chrome', $path );
-    print "Process authorization and paste code here\n";
-
-    my $code = readline(STDIN);
-    chomp($code);
-
-    $client->setHost('https://www.googleapis.com');
-    $client->POST(
-        '/oauth2/v4/token?'
-        . "code=4/$code&"
-        . "client_id=$CLIENT_ID&"
-        . 'redirect_uri=http://localhost:8080&'
-        . 'grant_type=authorization_code&'
-        . "client_secret=$CLIENT_SEC"
-    );
-    my $response = decode_json($client->responseContent());
-
-    $token = $response->{'access_token'};
+    $token = get_token();
 }
 
-my $client = REST::Client->new();
-$client->setHost('https://www.googleapis.com');
+print "TOKEN = <$token>\n";
 
-my $pages = {};
-my $response = get_pages( $client, $token );
-print_ids($response, $pages );
-
-my $next_token = $response->{'nextPageToken'};
-while ( defined($next_token) ) {
-    $response = get_pages( $client, $token, $next_token );
-    print_ids($response, $pages );
-    $next_token = $response->{'nextPageToken'};
-}
-
+my $page_ids = read_id_list( $PAGE_IDS ) if ( -f $PAGE_IDS );
+my $pages    = {};
 my @files;
+
 find (
     sub {
         if ( -f $File::Find::name && $File::Find::name =~ /blog_.+\.html/ ) {
@@ -76,6 +41,9 @@ find (
     },
     $HTML_PATH
 );
+
+my $client = REST::Client->new();
+$client->setHost('https://www.googleapis.com');
 
 foreach my $file ( @files ) {
     print "\nProcessing $file\n";
@@ -87,7 +55,7 @@ foreach my $file ( @files ) {
         'url'     => 'https://dnd-wod.blogspot.com/p/blog_' . $file . '.html'
     };
 
-    my $page_id = $pages->{$file};
+    my $page_id = get_id( $file );
     if ( defined($page_id) ) {
         $client->PUT(
             "blogger/v3/blogs/$BLOG_ID/pages/$page_id?access_token=$token",
@@ -125,6 +93,37 @@ foreach my $file ( @files ) {
 
 print "TOKEN = <$token>\n";
 
+sub get_token {
+    my $client = REST::Client->new();
+    $client->setHost('https://accounts.google.com');
+    $client->POST(
+        'o/oauth2/v2/auth?'
+        . 'scope=https://www.googleapis.com/auth/blogger&'
+        . 'redirect_uri=http://localhost:8080&'
+        . 'response_type=code&'
+        . "client_id=$CLIENT_ID"
+    );
+    my $path = $client->responseHeader('Location');
+    system( 'google-chrome', $path );
+    print "Process authorization and paste code here\n";
+
+    my $code = readline(STDIN);
+    chomp($code);
+
+    $client->setHost('https://www.googleapis.com');
+    $client->POST(
+        '/oauth2/v4/token?'
+        . "code=4/$code&"
+        . "client_id=$CLIENT_ID&"
+        . 'redirect_uri=http://localhost:8080&'
+        . 'grant_type=authorization_code&'
+        . "client_secret=$CLIENT_SEC"
+    );
+    my $response = decode_json($client->responseContent());
+
+    return $response->{'access_token'};
+}
+
 sub print_ids {
     my ($response, $pages) = @_;
 
@@ -156,6 +155,65 @@ sub get_pages {
         print "Responce code: " . $client->responseCode() . "\n";
         print "Responce body: " . $client->responseContent() . "\n";
     }
-    
+
     return decode_json($client->responseContent());
+}
+
+sub get_id {
+    my ( $name ) = @_;
+
+    my $id;
+    if ( exists( $page_ids->{$name} ) ) {
+        $id = $page_ids->{$name};
+    }
+    else {
+        my $client = REST::Client->new();
+        $client->setHost('https://www.googleapis.com');
+
+        my $response = get_pages( $client, $token );
+        print_ids( $response, $pages );
+
+        my $next_token = $response->{'nextPageToken'};
+        while ( defined($next_token) ) {
+            $response = get_pages( $client, $token, $next_token );
+            print_ids( $response, $pages );
+            $next_token = $response->{'nextPageToken'};
+        }
+        $page_ids = $pages;
+        save_id_list( $pages );
+        $id = $pages->{$name};
+    }
+
+    return $id;
+}
+
+sub save_id_list {
+    my ($pages) = @_;
+
+    open( my $pages_list, ">:utf8", "$PAGE_IDS") or die( "can't open $PAGE_IDS" );
+
+    foreach my $page ( keys %{ $pages } ) {
+        print $pages_list "$page => " . $pages->{$page} . "\n";
+    }
+
+    $pages_list->close();
+
+    return ;
+}
+
+sub read_id_list {
+    open( my $pages_list, "<:utf8", "$PAGE_IDS") or die( "can't open $PAGE_IDS" );
+
+    my $pages_ids = {};
+    my $line;
+    while (<$pages_list>) {
+        $line = $_;
+        chomp($line);
+        if ( $line =~ /^(.+) => (.+)/ ) {
+            $pages_ids->{$1} = $2;
+        }
+    }
+    $pages_list->close();
+
+    return $pages_ids;
 }
