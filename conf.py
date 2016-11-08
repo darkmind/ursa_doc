40bb1efd3b245ce9c1dd5c46ed33732b48b078f0
darkmind <nickolay.malkov@gmail.com>
2016-11-08 16:14:28 +0200

    Initial commit with build-tools and documentation

---
+++ conf.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import sys, os
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+    'sphinx.ext.viewcode', 'sphinx.ext.extlinks','sphinxcontrib.seqdiag',
+    'sphinxcontrib.blockdiag']
+
+todo_include_todos = True
+
+templates_path = ['_templates']
+html_theme_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = u'RPG system documentation'
+
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+html_theme = 'blog'
+
+html_add_permalinks = None
+
+html_short_title = project
+
+html_show_sphinx = False
---
+++ documentation/blog_Классы.rst
@@ -0,0 +1,23 @@
+======
+Классы
+======
+
+`Маг
+<https://dnd-wod.blogspot.com/p/blog-page_79.html>`_
+
+`Жрец
+<https://dnd-wod.blogspot.com/p/blog-page_38.html>`_
+
+`Воин
+<https://dnd-wod.blogspot.com/p/blog-page_69.html>`_
+
+**Классовые способности**
+
+`Сферы
+<https://dnd-wod.blogspot.com/p/blog-page_57.html>`_
+
+`Дисциплины
+<https://dnd-wod.blogspot.com/p/blog-page_35.html>`_
+
+`Дары
+<https://dnd-wod.blogspot.com/p/blog-page_8.html>`_
